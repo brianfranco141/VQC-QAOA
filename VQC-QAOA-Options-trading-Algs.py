bfbc831453e22b7880b9f51679e541d826ecee5a
Brian Franco <42521586+brianfranco141@users.noreply.github.com>
2022-12-05 04:51:06 -0800

    Update VQC-QAOA-Options-trading-Algs.py

--- VQC-QAOA-Options-trading-Algs.py
+++ VQC-QAOA-Options-trading-Algs.py
@@ -4,6 +4,11 @@ from qiskit.aqua.algorithms import VQC, QAOA
 from qiskit.aqua.components.optimizers import SPSA
 from qiskit.aqua.components.variational_forms import RY
 from qiskit.circuit.library import TwoLocal
+from alpaca_trade_api import REST
+
+API_KEY = "<your-api-key>"
+SECRET_KEY = "<your-secret-key>"
+api = REST(API_KEY, SECRET_KEY)
 
 # Define the number of qubits and the number of iterations for the VQC and QAOA algorithms.
 NUM_QUBITS = 5
@@ -22,46 +27,52 @@ def collect_data(option_contract_id):
     "implied_volatility": option.implied_volatility
   }
 
+  # Retrieve the dark pool orders for the option contract.
+  dark_pool_orders = api.orders(contract_id=option_contract_id, type="dark")
+
+  # Add the dark pool data to the dictionary.
+  data["dark_pool_volume"] = sum([order.quantity for order in dark_pool_orders])
+  data["dark_pool_routes"] = len(set([order.route.venue for order in dark_pool_orders]))
+
   return data
 
 def train_model(data):
   # Create the quantum circuit for the VQC or QAOA algorithm.
-  q = QuantumRegister(NUM_QUBITS)
-  c = ClassicalRegister(NUM_QUBITS)
-  qc = QuantumCircuit(q, c)
+  q = QuantumRegister(NUM_Q
 
-  # Use the VQC or QAOA algorithm to train the model.
-  if USE_VQC:
-    vqc = VQC(optimizer=SPSA(max_trials=100),
-              feature_map=TwoLocal(reps=3, entanglement="linear"),
-              data=data)
-    result = vqc.run(qc)
-  else:
-    qaoa = QAOA(optimizer=SPSA(max_trials=100),
-                feature_map=TwoLocal(reps=3, entanglement="linear"),
-                p=NUM_ITERATIONS,
-                data=data)
-    result = qaoa.run(qc)
+# Set the Hamiltonian for the VQC or QAOA algorithm.
+hamiltonian = TwoLocal(NUM_QUBITS, 'cz', 'cnot')
 
-  return result
+# Initialize the VQC or QAOA algorithm.
+if USE_VQC:
+  algorithm = VQC(hamiltonian, RY(NUM_QUBITS, depth=3), SPSA(max_trials=300))
+else:
+  algorithm = QAOA(hamiltonian, RY(NUM_QUBITS, depth=3), SPSA(max_trials=300), p=3)
+
+# Train the model on the data.
+result = algorithm.run(QuantumInstance(Aer.get_backend('statevector_simulator'),
+                                       shots=1024,
+                                       optimization_level=0))
+
+# Return the result of the model training.
+return result
 
 def execute_trades(option_contract_id, result):
-  # Collect the data for the option contract.
-  data = collect_data(option_contract_id)
+  # Use the trained model to make a prediction on the option data.
+  prediction = result.get_optimal_cost()
 
-  # Use the trained model to make a prediction on the data.
-  prediction = result.predict(data)[0]
-
+  # Place a trade based on the prediction.
   if prediction > THRESHOLD:
+    # Place a buy order for the option contract with a market price.
     api.submit_order(option_contract_id, 1, "buy", "market", None)
   else:
+    # Place a sell order for the option contract with a market price.
     api.submit_order(option_contract_id, 1, "sell", "market", None)
 
 def trade_options(option_contract_id):
-    # Collect the data for the option contract.
-    data = collect_data(option_contract_id)
-    # Train the model on the data.
-    result = train_model(data)
-    # Use the trained model to execute trades.
-    execute_trades(option_contract_id, result)
-
+  # Collect the data for the option contract.
+  data = collect_data(option_contract_id)
+  # Train the model on the data.
+  result = train_model(data)
+  # Use the trained model to execute trades.
+  execute_trades(option_contract_id, result)
