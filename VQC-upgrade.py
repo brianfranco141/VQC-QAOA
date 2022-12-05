ff5c70ce12839cdb3bf5a5c258de82cf1992d573
Brian Franco <42521586+brianfranco141@users.noreply.github.com>
2022-12-05 05:44:19 -0800

    Add files via upload

---
+++ VQC-upgrade.py
@@ -0,0 +1,88 @@
+from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
+from alpaca_trade_api import REST
+
+# Set the API key and secret key for Alpaca.
+API_KEY = "<your-api-key>"
+SECRET_KEY = "<your-secret-key>"
+
+# Create a REST client for Alpaca.
+api = REST(API_KEY, SECRET_KEY)
+
+# Set the threshold for the likelihood of the option price going up.
+THRESHOLD = 0.5
+
+# Load the historical data for the option contract.
+def load_data(contract_id):
+  data = api.option_chain(contract_id).chain
+  prices = []
+  large_position_detected = []
+  dark_pool_order_detected = []
+  momentum = []
+  for contract in data:
+    prices.append(contract.last_price)
+    large_position_detected.append(contract.large_position_detected)
+    dark_pool_order_detected.append(contract.dark_pool_order_detected)
+    momentum.append(contract.momentum)
+  return {"prices": prices, "large_position_detected": large_position_detected, "dark_pool_order_detected": dark_pool_order_detected, "momentum": momentum}
+
+# Train the VQC model on the data.
+def train_model(data):
+  # Split the data into training and testing sets.
+  n = len(data["prices"])
+  training_set = {"prices": data["prices"][:n//2], "large_position_detected": data["large_position_detected"][:n//2], "dark_pool_order_detected": data["dark_pool_order_detected"][:n//2], "momentum": data["momentum"][:n//2]}
+  testing_set = {"prices": data["prices"][n//2:], "large_position_detected": data["large_position_detected"][n//2:], "dark_pool_order_detected": data["dark_pool_order_detected"][n//2:], "momentum": data["momentum"][n//2:]}
+  
+  # Define the quantum circuit for the VQC model.
+  q = QuantumRegister(3)
+  c = ClassicalRegister(3)
+  qc = QuantumCircuit(q, c)
+  
+# Add the variational layers to the quantum circuit.
+  qc.u3(theta[0], theta[1], theta[2], q[0])
+  qc.cx(q[0], q[1])
+  qc.u3(theta[3], theta[4], theta[5], q[1])
+  qc.cx(q[0], q[1])
+  qc.u3(theta[6], theta[7], theta[8], q[1])
+  qc.cx(q[1], q[2])
+  qc.u3(theta[9], theta[10], theta[11], q[2])
+
+  # Measure the output qubits.
+  qc.measure(q[0], c[0])
+  qc.measure(q[1], c[1])
+  qc.measure(q[2], c[2])
+
+# Use the VQC model to make predictions on the testing set.
+predictions = []
+for i in range(len(testing_set["prices"])):
+  # Set the input data for the VQC model.
+  qc.data = [testing_set["prices"][i], testing_set["large_position_detected"][i], testing_set["dark_pool_order_detected"][i], testing_set["momentum"][i]]
+
+  # Run the VQC model on the input data.
+  result = qc.run(backend)
+
+  # Interpret the VQC model's output as a probability.
+  probability = result.get_counts()[0] / 100
+
+  # Make a prediction based on the probability.
+  if probability > THRESHOLD:
+    prediction = "UP"
+  else:
+    prediction = "DOWN"
+  predictions.append(prediction)
+
+# Place trades on profitable predictions.
+for i in range(len(predictions)):
+  if predictions[i] == "UP" and testing_set["prices"][i] < THRESHOLD:
+    # Place a buy order for the option contract.
+    order = api.place_order(contract_id, 1, "buy", "market", "day")
+    if order.status == "filled":
+      # Send an email notification.
+      send_email("Buy order filled for option contract ", contract_id)
+  elif predictions[i] == "DOWN" and testing_set["prices"][i] > THRESHOLD:
+    # Place a sell order for the option contract.
+    order = api.place_order(contract_id, 1, "sell", "market", "day")
+    if order.status == "filled":
+      # Send an email notification.
+      send_email("Sell order filled for option contract ", contract_id)
+
+
